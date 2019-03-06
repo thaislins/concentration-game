05903f1e57109b8fe1c1b83122397d4e36773022
Thais Lins <thaislin@gmail.com>
2019-03-05 22:48:35 -0300

    Flip card on click

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,18 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
     @IBAction func touchCard(_ sender: UIButton) {
         flipCard(withEmoji: "👻", on: sender)
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
+        let x = button.currentTitle
+        if x == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 }
 
