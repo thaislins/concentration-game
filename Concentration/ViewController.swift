2758f1b1a59e6e58d113605552e9aa19520544ae
Thais Lins <thaislin@gmail.com>
2019-03-06 01:31:36 -0300

    Add more cards Some other modifications

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var cards: Array<UIButton>!
     @IBOutlet weak var flipCountLabel: UILabel!
-    var mapCards = [UIButton : String]()
+    var emojiChoices = ["👻", "🧛🏼‍♀️", "🎃", "👻", "🧛🏼‍♀️", "🎃"]
     var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
@@ -21,22 +21,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for item in cards {
-            mapCards[item] = item.currentTitle
-        }
     }
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        flipCard(withEmoji: sender.currentTitle, on: sender)
+        if let cardNumber = cards.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
     }
     
     func flipCard(withEmoji emoji: String?, on button: UIButton) {
-        if button.currentTitle != "" {
+        if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         } else {
-            button.setTitle(mapCards[button], for: UIControl.State.normal)
+            button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
     }
