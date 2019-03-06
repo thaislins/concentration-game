a2afc8193c384c0177ce0df40b0e9ef5edcc1049
Thais Lins <thaislin@gmail.com>
2019-03-06 00:51:41 -0300

    Create map to associate card to its title

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var cards: Array<UIButton>!
     @IBOutlet weak var flipCountLabel: UILabel!
     var flipCount = 0
+    var mapCards = [UIButton : String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for item in cards {
+            mapCards[item] = item.currentTitle
+        }
     }
-    
+
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         flipCountLabel.text = "Flips: " + String(flipCount)
@@ -25,14 +29,12 @@ class ViewController: UIViewController {
     }
     
     func flipCard(withEmoji emoji: String?, on button: UIButton) {
-        print("flipCard(withEmoji: \(emoji))" )
         if button.currentTitle != "" {
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         } else {
-            button.setTitle(emoji, for: UIControl.State.normal)
+            button.setTitle(mapCards[button], for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
     }
 }
-
