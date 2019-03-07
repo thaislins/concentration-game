ceb08223acb339209047b185c6236b5d1358bc24
Thais Lins <thaislin@gmail.com>
2019-03-07 00:39:26 -0300

    Create card struct and concentration model

---
+++ Concentration/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Thais Lins on 2019-03-07.
+//  Copyright © 2019 Thais Lins. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    
+    var id : Int
+    var isFaceUp = false
+    var isMatched = false
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,18 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Thais Lins on 2019-03-07.
+//  Copyright © 2019 Thais Lins. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = Array<Card>()
+    
+    func chooseCard(at index: Int) {
+        
+    }
+}
